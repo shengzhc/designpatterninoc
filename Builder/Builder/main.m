ee22a5373b7fab4662b2f81b29a6d00da3570551
Shengzhe Chen <shengzhe.chen@gmail.com>
2015-01-30 10:57:35 -0800

    init

---
+++ Builder/Builder/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Builder
+//
+//  Created by Shengzhe Chen on 1/30/15.
+//  Copyright (c) 2015 Shengzhe Chen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
