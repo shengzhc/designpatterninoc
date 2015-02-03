b010b89016849c00cd5515d55c63b5f2db1c7ea2
Shengzhe Chen <schen@ancestry.com>
2015-02-03 10:36:43 -0800

    update

---
+++ Memento/Memento/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Memento
+//
+//  Created by Shengzhe Chen on 2/3/15.
+//  Copyright (c) 2015 Shengzhe Chen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
